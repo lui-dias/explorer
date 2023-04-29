80f983e02a9a0d55f34f5a69864cd5cdf4da4e4f
Luigi <luigidias@protonmail.com>
2023-04-29 12:59:46 -0300

    🔥 refactor: Remove arrow from contextmenu items

--- ui/src/components/ContextMenu/Sort/SortItem.svelte
+++ ui/src/components/ContextMenu/Sort/SortItem.svelte
@@ -1,10 +1,4 @@
 <script lang="ts">
-	import ArrowLeft from '../../icons/ArrowLeft.svelte'
 </script>
 
-<div class="flex justify-between items-center gap-x-3 w-full">
-	Sort by
-	<span class="transform rotate-180 w-1.5">
-		<ArrowLeft class="fill-primary" />
-	</span>
-</div>
+<div class="flex justify-between items-center gap-x-3 w-full">Sort by</div>
