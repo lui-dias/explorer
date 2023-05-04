7eeb512ef7438455837cb8c17dfdd0809913c907
Luigi <luigidias@protonmail.com>
2023-05-04 16:59:04 -0300

    🐛 fix: Define class ignore classes from variant

--- ui/src/components/ui/Button.svelte
+++ ui/src/components/ui/Button.svelte
@@ -6,23 +6,25 @@
 	const button = tv({
 		slots: {
 			border: 'p-0.5 bg-[linear-gradient(145deg,#4f5559,#131517)] rounded-full shadow-[6px_6px_15px_#24282a,_-6px_-6px_15px_#3d4347]',
-			inner: 'flex items-center justify-center w-11 h-11 rounded-full bg-[linear-gradient(145deg,#2d3134,#3f454a)] group',
-			icon: 'fill-primary group-disabled:fill-gray-400',
+			inner: 'flex items-center justify-center w-11 h-11 rounded-full bg-[linear-gradient(145deg,#2d3134,#3f454a)]',
 		},
 	})
 
 	const onClick = createEventDispatcher()
 
-	const { border, inner, icon } = button()
+	const { border, inner } = button()
 
 	// prettier-ignore
 	type $$Props = HTMLButtonAttributes;
 </script>
 
-<button type="button" class={border()} {...$$restProps} on:click={e => onClick('click', e)}>
+<button
+	type="button"
+	{...$$restProps}
+	class={`${border()} ${$$props.class}`}
+	on:click={e => onClick('click', e)}
+>
 	<div class={inner()}>
-		<span class={icon()}>
-			<slot name="icon" />
-		</span>
+		<slot name="icon" />
 	</div>
 </button>
