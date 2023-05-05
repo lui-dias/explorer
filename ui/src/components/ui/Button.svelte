ea779c760383fcdc818c3a10b47d472ce0f53a73
Luigi <luigidias@protonmail.com>
2023-05-05 09:39:00 -0300

    ✨ feat: Make button shadow optional

--- ui/src/components/ui/Button.svelte
+++ ui/src/components/ui/Button.svelte
@@ -2,19 +2,38 @@
 	import { createEventDispatcher } from 'svelte'
 	import type { HTMLButtonAttributes } from 'svelte/elements'
 	import { tv } from 'tailwind-variants'
+	import { sleep } from '../../utils'
 
 	const button = tv({
 		slots: {
 			inner: 'relative flex items-center justify-center w-11 h-11 rounded-full bg-[linear-gradient(145deg,#2d3134,#3f454a)] enabled:active:bg-[linear-gradient(145deg,#212426,#40464B)]',
-			after: 'after:w-[calc(100%+4px)] after:h-[calc(100%+4px)] after:absolute after:-z-10 after:bg-[linear-gradient(145deg,#4f5559,#131517)] after:rounded-full after:shadow-[6px_6px_15px_#24282a,_-6px_-6px_15px_#3d4347]',
+			after: 'after:w-[calc(100%+4px)] after:h-[calc(100%+4px)] after:absolute after:-z-10 after:bg-[linear-gradient(145deg,#4f5559,#131517)] after:rounded-full',
+		},
+		variants: {
+			shadow: {
+				true: {
+					after: 'after:shadow-[6px_6px_15px_#24282a,_-6px_-6px_15px_#3d4347]',
+				},
+				false: {
+					after: '',
+				},
+			},
 		},
 	})
 
-	type $$Props = HTMLButtonAttributes
+	type $$Props = HTMLButtonAttributes & {
+		shadow?: boolean
+	}
 
 	const onClick = createEventDispatcher()
 
-	const { inner, after } = button()
+	let { inner, after } = button({ shadow: $$restProps.shadow ?? true })
+
+	$: {
+		const _ = button({ shadow: $$restProps.shadow ?? true })
+		inner = _.inner
+		after = _.after
+	}
 </script>
 
 <button
