1303bed8e7c8a56bd9b7132ad6f81d5f242de0c2
Luigi <luigidias@protonmail.com>
2023-05-09 15:15:26 -0300

    ✨ feat: Go to disk on click

--- ui/src/components/Disks.svelte
+++ ui/src/components/Disks.svelte
@@ -1,46 +1,47 @@
 <script>
-	import { onMount } from 'svelte'
 	import { disks } from '../store'
 	import Icon from './ui/Icon.svelte'
-	import { formatBytes } from '../utils'
+	import { appendPath, formatBytes } from '../utils'
 </script>
 
-<ul class="space-y-6">
+<ul class="space-y-3">
 	{#each $disks as d}
-		<li class="group">
-			<div class="flex">
-				<div class="w-8">
-					<Icon icon="OtherDisk" glow={false} />
-				</div>
+		<li class="group h-12 overflow-hidden hover:h-24 transition-all ease-out duration-[400ms]">
+			<button type="button" on:click={() => appendPath(d.path)} class="w-full">
+				<div class="flex">
+					<div class="w-8">
+						<Icon icon="OtherDisk" glow={false} />
+					</div>
 
-				<div class="w-full flex flex-col ml-2 translate-y-1">
-					<span class="text-[#b9b9b9] font-bold leading-4">{d.device}</span>
+					<div class="w-full flex flex-col ml-2 translate-y-1">
+						<span class="text-[#b9b9b9] font-bold leading-4 text-left">{d.device}</span>
 
-					<div class="w-full flex justify-between">
-						<span class="text-[#7f8388] text-sm">{d.path}</span>
-						<span class="text-sm text-[#b9b9b9]">{d.percent}%</span>
+						<div class="w-full flex justify-between">
+							<span class="text-[#7f8388] text-sm">{d.path}</span>
+							<span class="text-sm text-[#b9b9b9]">{d.percent}%</span>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div class="mt-2">
-				<div class="flex h-1 group-hover:h-6 transition-all">
-					<div
-						class="w-full h-full rounded bg-[rgba(255,255,255,0.5)] text-center font-medium text-white overflow-hidden relative"
-					>
-						<div class="h-full bg-blue-500" style={`width: ${d.percent}%`} />
-						<span class="block absolute inset-0 top-0.5 text-sm">
-							{formatBytes(d.used)} Used
-						</span>
+				<div class="mt-2">
+					<div class="flex h-1 group-hover:h-6 transition-all duration-[400ms]">
+						<div
+							class="w-full h-full rounded bg-[rgba(255,255,255,0.5)] text-center font-medium text-white overflow-hidden relative"
+						>
+							<div class="h-full bg-blue-500" style={`width: ${d.percent}%`} />
+							<span class="block absolute inset-0 top-0.5 text-sm">
+								{formatBytes(d.used)} Used
+							</span>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div class="text-sm text-[#b9b9b9] font-bold mt-1">
-				{formatBytes(d.free)}
-				<span class="text-sm text-[#7f8388] font-normal"> free of </span>
-				{formatBytes(d.total)}
-			</div>
+				<div class="text-sm text-[#b9b9b9] text-left font-bold mt-1 opacity-0 group-hover:opacity-100 transition-opacity">
+					{formatBytes(d.free)}
+					<span class="text-sm text-[#7f8388] font-normal"> free of </span>
+					{formatBytes(d.total)}
+				</div>
+			</button>
 		</li>
 	{/each}
 </ul>
