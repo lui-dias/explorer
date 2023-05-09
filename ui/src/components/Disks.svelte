bbfd5b3e350ae6cacea43e440a818b0a3fef9029
Luigi <luigidias@protonmail.com>
2023-05-09 14:46:25 -0300

    ✨ feat: Add better disk usage texts

--- ui/src/components/Disks.svelte
+++ ui/src/components/Disks.svelte
@@ -24,16 +24,22 @@
 			</div>
 
 			<div class="mt-2">
-				<div class="flex h-1">
+				<div class="flex h-1 group-hover:h-6 transition-all">
 					<div
-						class="w-full h-full rounded bg-[rgba(255,255,255,0.5)] transition-all group-hover:h-6 text-center font-medium text-white overflow-hidden relative"
+						class="w-full h-full rounded bg-[rgba(255,255,255,0.5)] text-center font-medium text-white overflow-hidden relative"
 					>
 						<div class="h-full bg-blue-500" style={`width: ${d.percent}%`} />
 						<span class="block absolute inset-0 top-0.5 text-sm">
-							{formatBytes(d.used)}/{formatBytes(d.total)}
+							{formatBytes(d.used)} Used
 						</span>
 					</div>
 				</div>
+			</div>
+
+			<div class="text-sm text-[#b9b9b9] font-bold mt-1">
+				{formatBytes(d.free)}
+				<span class="text-sm text-[#7f8388] font-normal"> free of </span>
+				{formatBytes(d.total)}
 			</div>
 		</li>
 	{/each}
