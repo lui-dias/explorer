c34fafb37c006e12df3a08589211aa8b79754ed5
Luigi <luigidias@protonmail.com>
2023-05-15 13:02:31 -0300

    🐛 fix: Cant select text preview

--- ui/src/components/Preview.svelte
+++ ui/src/components/Preview.svelte
@@ -191,9 +191,10 @@
 </svelte:head>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div on:click={() => {
-    isExplorerFocused.set(false)
-}}
+<div
+	on:click={() => {
+		isExplorerFocused.set(false)
+	}}
 	class={`_preview w-full transition-all duration-300 ease-out h-[398px] pl-4 overflow-y-auto scrollbar-thin scrollbar-thumb-zinc-700 ${
 		$selected.length === 1 && $selected[0].kind === 'file' ? 'max-w-[250px]' : 'max-w-0'
 	}`}
@@ -220,15 +221,15 @@
 					{/if}
 					<div>
 						{#each [12, 16, 24, 32, 48, 64] as size}
-                            <div class="flex items-center gap-x-2">
-                                <p class="text-sm">{size}</p>
-                                <p
-                                    class="font-thin whitespace-nowrap"
-                                    style={`font-family: ${font.actual}; font-weight: ${weight}; font-size: ${size}px`}
-                                >
-                                    The quick brown fox jumps over the lazy dog
-                                </p>
-                            </div>
+							<div class="flex items-center gap-x-2">
+								<p class="text-sm">{size}</p>
+								<p
+									class="font-thin whitespace-nowrap"
+									style={`font-family: ${font.actual}; font-weight: ${weight}; font-size: ${size}px`}
+								>
+									The quick brown fox jumps over the lazy dog
+								</p>
+							</div>
 						{/each}
 					</div>
 				</div>
@@ -240,7 +241,7 @@
 			<Highlight
 				language={type.language}
 				code={type.text}
-				class="pr-2 overflow-auto h-full scrollbar-thin scrollbar-thumb-zinc-700 [&>*]:scrollbar-thin [&>*]:scrollbar-thumb-zinc-700 [&>*]:h-full"
+				class="pr-2 overflow-auto h-full scrollbar-thin scrollbar-thumb-zinc-700 [&>*]:scrollbar-thin [&>*]:scrollbar-thumb-zinc-700 [&>*]:h-full [&>*]:select-text"
 			/>
 		{/if}
 	{/if}
