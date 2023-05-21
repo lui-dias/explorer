6be3fb7893add43c551091c7104125705dd29afe
Luigi <luigidias@protonmail.com>
2023-05-21 14:24:43 -0300

    🐛 fix: Weird html of Pin icon

--- ui/src/components/icons/contextmenu/Pin.svelte
+++ ui/src/components/icons/contextmenu/Pin.svelte
@@ -1,4 +1,4 @@
-class={$$props.class}<svg width="12" height="20" xmlns="http://www.w3.org/2000/svg"
+<svg width="12" height="20" xmlns="http://www.w3.org/2000/svg" class={$$props.class}
 	><path
         fill="var(--primary)"
 		d="m10 9 2 2v2H7v6l-1 1-1-1v-6H0v-2l2-2V2H1V0h10v2h-1zm-7.15 2h6.3L8 9.85V2H4v7.85ZM6 11Z"
