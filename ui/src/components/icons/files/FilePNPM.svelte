a7945d1568590dd5e65638aceff539c322a4648d
Luigi <luigidias@protonmail.com>
2023-05-23 16:31:16 -0300

    🔥 refactor: Remove unused components

--- ui/src/components/icons/files/FilePNPM.svelte
+++
@@ -1,9 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24"
-	><path
-		d="M30 10.75h-8.75V2H30ZM20.37 10.75h-8.75V2h8.75ZM10.75 10.75H2V2h8.75ZM30 20.38h-8.75v-8.75H30Z"
-		style="fill:#f9ad00"
-	/><path
-		d="M20.37 20.38h-8.75v-8.75h8.75ZM20.37 30h-8.75v-8.75h8.75ZM30 30h-8.75v-8.75H30ZM10.75 30H2v-8.75h8.75Z"
-		style="fill:#fff"
-	/></svg
->
--- ui/src/components/icons/files/FileToml.svelte
+++
@@ -1,6 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24"
-	><path d="M22.76 6.83v3.25h-5v15.09h-3.5V10.08h-5V6.83Z" style="fill:#7f7f7f" /><path
-		d="M2 2h6.2v3.09H5.34v21.8H8.2V30H2ZM30 30h-6.2v-3.09h2.86V5.11H23.8V2H30Z"
-		style="fill:#bfbfbf"
-	/></svg
->
