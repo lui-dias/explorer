79b62e9900a004f7a7f652da458a94a1c4bdc547
Luigi <luigidias@protonmail.com>
2023-05-24 08:33:53 -0300

    🐛 fix: Wrong %ProgramFiles% replace

--- main.py
+++ main.py
@@ -2259,21 +2259,30 @@ class API:
 
     def parse_path(self, path: str):
         user = getuser()
+        is_64bits = sys.maxsize > 2**32
 
         a = re.sub(r'%AllUsersProfile%', 'C:/ProgramData', path, flags=re.I)
         a = re.sub(r'%AppData%', f'C:/Users/{user}/AppData/Roaming', a, flags=re.I)
-        a = re.sub(r'%CommonProgramFiles%', 'C:/Program Files/Common Files', a, flags=re.I)
-        a = re.sub(r'%CommonProgramFiles(x86)%', 'C:/Program Files (x86)', a, flags=re.I)
+        a = re.sub(
+            r'%CommonProgramFiles%', 'C:/Program Files/Common Files', a, flags=re.I
+        )
+        a = re.sub(
+            r'%CommonProgramFiles(x86)%', 'C:/Program Files (x86)', a, flags=re.I
+        )
         a = re.sub(r'%HomeDrive%', 'C:/', a, flags=re.I)
         a = re.sub(r'%LocalAppData%', f'C:/Users/{user}/AppData/Local', a, flags=re.I)
         a = re.sub(r'%ProgramData%', 'C:/ProgramData', a, flags=re.I)
-        a = re.sub(r'%ProgramFiles%', 'C:/Program Files or C:/Program Files (x86)', a, flags=re.I)
+        a = re.sub(
+            r'%ProgramFiles%',
+            'C:/Program Files' if is_64bits else 'C:/Program Files (x86)',
+            a,
+            flags=re.I,
+        )
         a = re.sub(r'%Public%', 'C:/Users/Public', a, flags=re.I)
         a = re.sub(r'%SystemDrive%', 'C:', a, flags=re.I)
         a = re.sub(r'%SystemRoot%', 'C:/Windows', a, flags=re.I)
         a = re.sub(r'%UserProfile%', f'C:/Users/{user}', a, flags=re.I)
 
-        
         return Path(a).as_posix()
 
 
